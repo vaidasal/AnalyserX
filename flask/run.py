332f317ee488aa623a9c2caadca0561c80faffa3
Vaidas Alaune <vaidasal@gmail.com>
2021-05-17 14:46:46 +0200

    Added nginx wsgi server

---
+++ flask/run.py
@@ -0,0 +1,6 @@
+from analyser import app
+
+if __name__ == "__main__":
+    app.run(use_reloader=False, host='0.0.0.0')
+
+
--- run.py
+++
@@ -1,6 +0,0 @@
-from analyser import app
-
-if __name__ == "__main__":
-    app.run(use_reloader=False, host="0.0.0.0")
-    print("run")
-
